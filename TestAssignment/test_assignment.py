1335bc565a3b13d7945fe05d62f3e4a86f88c9db
ronit-verma <67920527+ronit-verma@users.noreply.github.com>
2020-08-10 15:57:28 +0530

    updating code for testing API

--- TestAssignment/test_assignment.py
+++ TestAssignment/test_assignment.py
@@ -1,5 +1,6 @@
 import time
 
+import requests
 from selenium import webdriver
 
 def setup():
@@ -10,6 +11,7 @@ def setup():
     driver.maximize_window()
     driver.implicitly_wait(10)
 
+
 def teardown():
     print('Inside teardown method')
     driver.quit()
@@ -19,6 +21,7 @@ def test_validate_Phase1_UI():
     print('inside test')
 
     driver.get("https://social.ndtv.com/static/Weather/report/")
+
 
     #sending cityname
     cityName = driver.find_element_by_xpath("//input[@class='searchBox']").send_keys('Kanpur')
@@ -38,7 +41,18 @@ def test_validate_Phase1_UI():
 
 
 def test_validate_Phase2_API():
-    
-    driver.get("https://social.ndtv.com/static/Weather/report/")
+
+  driver.get("http://api.openweathermap.org/data/2.5/weather?q=Kanpur&appid=7fe67bf08c80ded756e598d6f8fedaea")
+  resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Kanpur&appid=7fe67bf08c80ded756e598d6f8fedaea')
+  assert  resp.status_code == 200
+  assert resp.json()["name"] == 'Kanpur'
+  print(resp.text)
+
+#def test_compare_API_UI_weather():
+
+
+
+
+
 
 
