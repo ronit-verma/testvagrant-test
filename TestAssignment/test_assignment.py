b2cc5986520dfc0d0ee2724b0e6559443bfac148
ronit-verma <67920527+ronit-verma@users.noreply.github.com>
2020-08-10 14:56:33 +0530

    code for phase1
    commiting code for phase 1

--- TestAssignment/test_assignment.py
+++ TestAssignment/test_assignment.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 def setup():
@@ -12,10 +14,31 @@ def teardown():
     print('Inside teardown method')
     driver.quit()
 
-def test_validate_Phase1():
+def test_validate_Phase1_UI():
 
     print('inside test')
 
-    driver.get("https://www.ndtv.com/")
+    driver.get("https://social.ndtv.com/static/Weather/report/")
 
-    #menu = driver.find_element_by_xpath("//*[@id='suggestion-search']")
+    #sending cityname
+    cityName = driver.find_element_by_xpath("//input[@class='searchBox']").send_keys('Kanpur')
+
+    time.sleep(4)
+
+    #click on checkbox
+
+    cityCheckBox = driver.find_element_by_xpath("//div[42]//label[1]//input[1]").click()
+
+    cityOnMap = driver.find_element_by_xpath("//*[@id='map_canvas']/div[1]/div[4]/div[12]/div/div[2]")
+    print(cityOnMap.text)
+
+    assert("Kanpur" in cityOnMap.text)
+    cityOnMap.click()
+    driver.save_screenshot('citydetails.png')
+
+
+def test_validate_Phase2_API():
+    
+    driver.get("https://social.ndtv.com/static/Weather/report/")
+
+
